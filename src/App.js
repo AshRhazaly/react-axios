9836ebc209d73138433afd48f19752d6fb9e0b3b
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-05-04 16:22:03 +0800

    Add conditional statement if no gifs are rendered

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     this.performSearch();
   }
 
-  performSearch = (query = "cats") => {
+  performSearch = (query = "girls") => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
